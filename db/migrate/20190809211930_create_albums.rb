2e165b8874ff319144c40261d6c9368c68cb0656
Liliana_Rache <lilianarache@gmail.com>
2019-08-09 17:51:18 -0500

    add columns in the migrations

--- db/migrate/20190809211930_create_albums.rb
+++ db/migrate/20190809211930_create_albums.rb
@@ -1,7 +1,9 @@
 class CreateAlbums < ActiveRecord::Migration[5.2]
   def change
     create_table :albums do |t|
-
+      t.string :name
+      t.text :image_url
+      t.string :released_at
       t.timestamps
     end
   end
